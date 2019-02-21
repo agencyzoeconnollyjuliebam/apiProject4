404b5215d450835a33228633c411efcf7338594b
Julie <julie.bam1985@gmail.com>
2019-02-21 17:56:37 -0500

    got images to print to page

--- script/script.js
+++ script/script.js
@@ -7,13 +7,17 @@ adoptApp.apiKeySecret ='d2daaca6e8ab78541a2dce99582e5aa0';
 //adoptApp.apiUrl = `http://api.petfinder.com/pet.find?key=${adoptApp.apiKey}&location=city`;
 adoptApp.apiUrl = `http://api.petfinder.com/pet.find`;
 
+//Our document ready
+$(document).ready(function(){
+    adoptApp.init();
+})
 
-//hold all functions for the app
-// adoptApp.init = function (){
-//     adoptApp.getItems(location);
-    // adoptApp.userInput();
-//}
+//Initializing the app
+adoptApp.init = function(){
+    adoptApp.location();
+}
 
+//Making Ajax call with userInput
 adoptApp.getItems = function(location){
     $.ajax({
         url: adoptApp.apiUrl,
@@ -28,11 +32,17 @@ adoptApp.getItems = function(location){
             format: 'json'
         }
     }).then((results) => {
+        // This is where we put code that will print the data to page
+        adoptApp.print(results.petfinder.pets.pet);
+
+
+
+
         console.log(results.petfinder.pets.pet)//TBD
     })
 }
 
-// adoptApp.getItems();
+//Users input is acquired here
 adoptApp.location = function (){
     $('form').on('submit', function(e){
         e.preventDefault();
@@ -43,25 +53,41 @@ adoptApp.location = function (){
     }) 
 }
 
-$(document).ready(function(){
-    adoptApp.init();
-})
 
+// take result from ajax call and print to page
 
-adoptApp.init = function(){
-    adoptApp.location();
-    // adoptApp.getItems();
+adoptApp.print = function (pets){
+    pets.forEach(function(pet) {
+        if(pet.media.photos.photo) {
+            const images = $(`<img>`).attr('src', pet.media.photos.photo[2].$t);
+        // for (let i = 0; i < 0; i ++) {
+        //     //We need to create a const that will hold individual photos of each pet item of the ajax array
+
+        //     const individualImage = $(pets).
+
+            console.log(pet.media.photos.photo[0].$t)
+            $('.results .gallery').append(`<div class="petCrate"></div>`)
+            $('.results .gallery .petCrate').append(`<div class="petImage"></div>`)
+            $('.results .gallery .petCrate .petImage').append(images)
+        }
+    })
 }
- //create variable to hold users input(location)
 
-// make an ajax request to petFinder API with user's input (location)
+//when user clicks on image , we get a pop-up message with all the pet's info
 
 
-//create variables for the user's pet choice (cat/dog)
 
-//take location object and filter the object based on user pet choice
+//need to extract image from ajax object and populate in html gallery
+// artApp.displayArt = function(pieces) {
+//     pieces.forEach(function(piece) {
+//      if(piece.hasImage === true){
+//         const title = $('<h2>').text(piece.title);
+//         const artist = $('<p>').addClass('artist').text(piece.principalOrFirstMaker);
+//         const image = $('<img>').attr('src', piece.webImage.url);
+//         const artPiece = $('<div>').addClass('piece').append(image, title, artist);
+//         $('#artwork').append(artPiece);
+//      }
+//     })
+// }
 
-//On submit, run function and print to page relative pet infrmation - img, cat/dog, adoption status, size, shelter ID
 
-//notes:
-// on submit get the usersInput value  of location  and inserted in the ajx call data
